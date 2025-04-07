ceb6f675c12b58d219c5eb22278668a3027fb6ca
GATESJM24 <gatesjmwithaG@gmail.com>
2025-04-07 15:30:06 -0400

    Added Classes with stubs

---
+++ src/Pokemon.java
@@ -0,0 +1,51 @@
+public class Pokemon {
+
+    public Pokemon(){
+        //stub
+    }
+
+    public String[] getTypes(int types){
+        //stub
+        return null;
+    }
+
+    public int getMAxHealth(){
+        //stub
+        return -1;
+    }
+
+    public int getCurrentHealth(){
+        //stub
+        return -1;
+    }
+
+    public int getHealthStat(){
+        //stub
+        return -1;
+    }
+
+    public int getDamageStat(){
+        //stub
+        return -1;
+    }
+
+    public int getDefenceStat(){
+        //stub
+        return -1;
+    }
+
+    public int getSDamageStat(){
+        //stub
+        return -1;
+    }
+
+    public int getSDefenceStat(){
+        //stub
+        return -1;
+    }
+
+    public int getSpeedStat(){
+        //stub
+        return -1;
+    }
+}
